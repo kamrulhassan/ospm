42439a0ad24ab21f4f3c57da615607f7eb364a98
RaceBoss <raceboss@ubuntu.ubuntu-domain>
2012-05-05 13:58:10 +0200

    OSPM - 0.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ospm::Application.config.secret_token = 'ae7e11544c81e3868d2f968ddbd875d7acf915de98afce256763a62ea2533cbf5f75c3f9f448980980654e11fcf2607195eb32bf572a7b7c5032f6856bce81b6'
