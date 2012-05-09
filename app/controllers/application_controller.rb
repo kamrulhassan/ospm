1da1cbd52737b1a61b439716a60527d2bcfe90bb
Kamrul Hassan <kamrul3d@gmail.com>
2012-05-09 11:30:59 +0200

    assign appropriate admin role to repository user when publishing project from plan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,27 +30,40 @@ end
 def create_repo_project(plan_project)
 	@project=Project.create(:name => plan_project.name, :description => plan_project.description)
 
-	create_user_for_repository(current_plan_user.email, :admin, @project)
+	# create_user_for_repository(current_plan_user.email, :admin, @project)
+
+	@plan_project_creator=User.find_by_email(current_plan_user.email)
+	@plan_project_creator=User.create({:email => "#{current_plan_user.email}", :password => "111111", :password_confirmation => "111111" }) unless @plan_project_creator
+
+	assign_role(@plan_project_creator, :admin, @project)
 
 	plan_project_workpackages = plan_project.plan_workpackages
 
 	plan_project_workpackages.each do |plan_workpackage|
+
 		plan_elements=plan_workpackage.plan_elements
-			plan_elements.each do |plan_element|
-					# insert deliverable
-					@repo_deliverable=Deliverable.create(:description => plan_element.description, :start_date => plan_element.start_date, :end_date => plan_element.end_date, :project_id => @project.id)
-					plan_element.plan_resources.each do | assigneduser|
-						# insert user and assign admin role to deliverables
-						create_user_for_repository("#{assigneduser.name}@ospm.com",:admin, @repo_deliverable)
-					end
-			end
+
+		plan_elements.each do |plan_element|
+				# insert deliverable
+				@repo_deliverable=Deliverable.create(:description => plan_element.description, :start_date => plan_element.start_date, :end_date => plan_element.end_date, :project_id => @project.id)
+
+				plan_element.plan_resources.each do | assigneduser|
+					# insert user and assign admin role to deliverables
+					create_user_for_repository("#{assigneduser.name}@ospm.com",:admin, @repo_deliverable)
+				end
+				assign_role(@plan_project_creator, :read, @repo_deliverable)
+		end
 	end
 end
 
 def create_user_for_repository(email, role, instance)
 	user=User.find_by_email(email)
 	user=User.create({:email => "#{email}", :password => "111111", :password_confirmation => "111111" }) unless user
-	user.add_role role, instance unless user.has_role? role, instance
+	assign_role(user,role,instance)
+	# user.add_role role, instance unless user.has_role? role, instance
 end
 
+def assign_role(user, role, instance)
+	user.add_role role, instance unless user.has_role? role, instance
+end
 end
